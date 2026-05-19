aede03ae0b17580eede4263c6bc55f3cd7fe3311
franklin <kevin.kim@bestbytes.com>
2026-05-19 13:36:59 +0200

    feat(menubar): wire main and make targets

--- cmd/dockprox-menubar/main.go
+++ cmd/dockprox-menubar/main.go
@@ -3,12 +3,23 @@
 package main
 
 import (
-	"log"
+	"os"
 
+	"github.com/charmbracelet/log"
+	"github.com/foomo/dockprox/internal/menubar"
 	"github.com/wailsapp/wails/v3/pkg/application"
 )
 
 func main() {
+	logger := log.NewWithOptions(os.Stderr, log.Options{ReportTimestamp: true})
+
+	cfgPath, err := menubar.Resolve()
+	if err != nil {
+		logger.Fatal("config resolve", "err", err)
+	}
+
+	ctrl := menubar.New(cfgPath, logger)
+
 	app := application.New(application.Options{
 		Name: "dockprox",
 		Mac: application.MacOptions{
@@ -16,7 +27,15 @@ func main() {
 		},
 	})
 
+	menubar.NewTray(app, ctrl, logger)
+
+	if err := ctrl.Start(); err != nil {
+		logger.Warn("auto-start failed", "err", err)
+	}
+
+	app.OnShutdown(func() { _ = ctrl.Stop() })
+
 	if err := app.Run(); err != nil {
-		log.Fatal(err)
+		logger.Fatal("app run", "err", err)
 	}
 }
