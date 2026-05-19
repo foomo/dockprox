2e25fc51ff896a42e6f7010ed16e2c05dce9cda5
franklin <kevin.kim@bestbytes.com>
2026-05-19 14:49:09 +0200

    test(menubar): add reveal command test

---
+++ internal/menubar/reveal_test.go
@@ -0,0 +1,15 @@
+//go:build safe && darwin
+
+package menubar //nolint:testpackage // needs access to unexported buildRevealCommand
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildRevealCommand(t *testing.T) {
+	cmd := buildRevealCommand("/some/path/config.yaml")
+	assert.Equal(t, "open", cmd.Args[0])
+	assert.Equal(t, []string{"open", "-R", "/some/path/config.yaml"}, cmd.Args)
+}
